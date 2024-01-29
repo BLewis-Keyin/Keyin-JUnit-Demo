2876ac8054ee134112973f0aae40e5c0a8d43ae1
BL-Keyin <you@example.com>
2024-01-29 00:59:40 -0500

    test

--- src/test/java/com/keyin/SuggestionEngineTest.java
+++ src/test/java/com/keyin/SuggestionEngineTest.java
@@ -1,40 +1,26 @@
 package com.keyin;
 
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
+
 import java.util.HashMap;
 import java.util.Map;
-import java.net.URL;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 public class SuggestionEngineTest {
-
-    private SuggestionEngine suggestionEngine;
+    private SuggestionEngine suggestionEngine = new SuggestionEngine();
 
     @Mock
     private SuggestionsDatabase mockSuggestionDB;
-
-    @BeforeEach
-    public void setUp() throws Exception {
-        suggestionEngine = new SuggestionEngine();
-        URL resource = getClass().getClassLoader().getResource("words.txt");
-        if (resource == null) {
-            throw new IllegalArgumentException("file not found!");
-        } else {
-            Path path = Paths.get(resource.toURI());
-            suggestionEngine.loadDictionaryData(path);
-        }
-    }
-
+    private boolean testInstanceSame = false;
 
 
     @Test
@@ -68,11 +54,6 @@ public class SuggestionEngineTest {
         assertEquals("", suggestionEngine.generateSuggestions(largeInput));
     }
 
-    @Test
-    public void testLoadingOfDictionaryData() {
-        assertNotNull(suggestionEngine.getWordSuggestionDB());
-        assertFalse(suggestionEngine.getWordSuggestionDB().isEmpty());
-    }
 
     @Test
     public void testCaseInsensitivity() {
